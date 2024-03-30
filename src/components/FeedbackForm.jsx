35bec39182c6f8f6e38cc46ecdd9f60adf247bed
amine-dh <amine.dahah.etudes@gmail.com>
2024-03-30 02:53:08 +0100

    initial version

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,80 @@
+import Card from './shared/Card'
+import { useState, useContext, useEffect } from 'react'
+import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
+
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Your review is too short')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+
+    setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      if (feedbackEdit.edit) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+      
+
+      setText('')
+    }
+  }
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How Would Rate your service with us</h2>
+        <RatingSelect select={setRating} selected={rating} />
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='Write a review'
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button isDisabled={btnDisabled} type='submit'>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
